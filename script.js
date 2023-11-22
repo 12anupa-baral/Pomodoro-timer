bbf72fc4b023bb4a63a4bac31daece79b8118364
Anupa Baral <baralanupa988@gmail.com>
2023-11-22 17:53:07 +0545

    modal added

--- script.js
+++ script.js
@@ -127,3 +127,40 @@ document
   });
 
 updateDisplay();
+
+// const modal = document.getElementById("modal");
+// const theme = document.getElementById("theme");
+
+// theme.addEventListener("onclick", function () {
+//   modal.style.display = "block";
+//   container.style.background = "rgba(0, 0, 0, 0.5)";
+// });
+
+const clickHere = document.getElementById("clickHere");
+const modal = document.getElementById("modal");
+const container = document.getElementById("container");
+
+function closeModal() {
+  modal.style.display = "none";
+  container.style.background = "";
+}
+
+clickHere.addEventListener("click", function () {
+  modal.style.display = "block";
+  // container.style.background = "rgba(0, 0, 0, 0.5)";
+});
+
+const closeElements = [
+  modal,
+  document.getElementById("downButton"),
+  document.getElementById("cancel"),
+  document.getElementById("icon"),
+];
+
+closeElements.forEach((element) => {
+  element.addEventListener("click", function (event) {
+    if (event.target === element || event.target === modal) {
+      closeModal();
+    }
+  });
+});
